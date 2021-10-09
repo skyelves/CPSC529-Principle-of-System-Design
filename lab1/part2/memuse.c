15b1c64586497504f8c3caa887ea22c0fb325ad5
skyelves <1600012805@pku.edu.cn>
2021-10-09 00:49:34 -0400

    part2

--- lab1/part2/memuse.c
+++ lab1/part2/memuse.c
@@ -7,5 +7,7 @@ SYSCALL_DEFINE2(memuse_syscall, pid_t, pid, int, memory_type){
         struct pid* res = find_vpid(pid);
         rcu_read_unlock();
         printk("pid:%d, memory_type:%d\n", pid, memory_type);
+        if((memory_type > 3) || (memory_type < 1))
+                return -1;
         return (res == NULL ? -1 : 1);
 }
