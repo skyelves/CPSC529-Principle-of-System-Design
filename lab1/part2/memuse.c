2010e3d3408810d02403c141eb78e53276ddb7d2
skyelves <1600012805@pku.edu.cn>
2021-10-10 13:54:58 -0400

    part2

--- lab1/part2/memuse.c
+++ lab1/part2/memuse.c
@@ -3,10 +3,15 @@
 #include <linux/pid.h> 
 
 SYSCALL_DEFINE2(memuse_syscall, pid_t, pid, int, memory_type){
+        // acquite the rcu lock
         rcu_read_lock();
+        //check if the process exists
         struct pid* res = find_vpid(pid);
+        // release the rcu lock
         rcu_read_unlock();
+        // print the info
         printk("pid:%d, memory_type:%d\n", pid, memory_type);
+        // check the memory type
         if((memory_type > 3) || (memory_type < 1))
                 return -1;
         return (res == NULL ? -1 : 1);
