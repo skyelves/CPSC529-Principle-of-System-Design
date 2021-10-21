4dad444622f4232f8f46188bc33fec8cf231ad77
skyelves <1600012805@pku.edu.cn>
2021-10-21 14:25:53 -0400

    part4

--- lab1/part4/user/user.c
+++ lab1/part4/user/user.c
@@ -22,7 +22,7 @@ void test_syscall(pid_t _pid, int _type){
 }
 
 void *kernel_stack_test_func(void* args) {
-	int i = 1;
+    int i = 1;
     sleep(1);
 }
 
@@ -41,14 +41,21 @@ int main()
     for (int i = 0; i < 3; i++){
         test_syscall(pid, memory_type[i]);
     }
+    printf("\n");
 
     // test heap
+    printf("Heap tests start:\n");
+    printf("malloc 16 bytes.\n");
     char *tmp0 = (char *)malloc(16);
     test_syscall(pid, 3);
+    printf("malloc 4096 bytes.\n");
     char *tmp1 = (char *)malloc(4096);
     test_syscall(pid, 3);
+    printf("Heap tests end.\n\n");
 
     // test kernel stack, each thread has a kernel stack
+    printf("Kernel stack tests start:\n");
+    printf("create %d threads.\n", TEST_THREAD_NUM);
     pthread_t pt[TEST_THREAD_NUM];
     for (size_t i = 0; i < TEST_THREAD_NUM; i++){
         pthread_create(&pt[i], NULL, kernel_stack_test_func, NULL);
@@ -57,10 +64,13 @@ int main()
     for (size_t i = 0; i < TEST_THREAD_NUM; i++){
         pthread_join(pt[i], NULL);
     }
+    printf("Kernel stack tests end.\n\n");
 
     // test user stack
+    printf("User stack tests start:\n");
+    printf("Recursive function stops until depth == 512.\n");
     user_stack_test_func(0);
-
+    printf("User stack tests end.\n\n");
     
     return 0;
 }
